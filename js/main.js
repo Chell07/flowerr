8b63727dc22202579c1b5ccf95ab5d3ed2b97269
Chell07 <148919000+Chell07@users.noreply.github.com>
2024-12-26 17:17:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Love you, Felaa').split('')
+    const titles = ('Love you').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
